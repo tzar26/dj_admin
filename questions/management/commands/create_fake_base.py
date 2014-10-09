9eb506ee156ce1f7f5a913c63e3b02b28c243336
Stepkin Alezander <stepkin@jetstyle.ru>
2014-10-09 17:42:14 +0600

    устранено повторное наполнение фейковыми вопросами/категориями

--- questions/management/commands/create_fake_base.py
+++ questions/management/commands/create_fake_base.py
@@ -9,19 +9,26 @@ class Command(BaseCommand):
     questions_pool_length = 20
 
     def handle(self, *args, **options):
-        for i in range(0, self.categories_pool_length):
-            category = Category(name = self.categories_prefix + str(i + 1) )
-            category.save()
-        import random
-        for i in range(0, self.questions_pool_length):
-            a = random.randint(0,20)
-            b = random.randint(0,20)
-            answer = a+b
-            question_text = '%s %s. Введите правильный ответ в выражении %s+%s' % (self.questions_prefix, i, a, b)
-            question = Question(
-                category = Category.objects.get(id = random.randint(1, self.categories_pool_length)),
-                question = question_text,
-                answer = str(answer)
-            )
-            question.save()
-        print 'Фейковая база наполнена'
+        if Category.objects.count() < 1:
+            for i in range(0, self.categories_pool_length):
+                category = Category(name = self.categories_prefix + str(i + 1) )
+                category.save()
+            print 'Таблица категорий наполнена'
+        else:
+            print 'Таблица категорий не пустая. Создание данных пропускается'
+        if Question.objects.count() < 1:
+            import random
+            for i in range(0, self.questions_pool_length):
+                a = random.randint(0,20)
+                b = random.randint(0,20)
+                answer = a+b
+                question_text = '%s %s. Введите правильный ответ в выражении %s+%s' % (self.questions_prefix, i, a, b)
+                question = Question(
+                    category = Category.objects.get(id = random.randint(1, self.categories_pool_length)),
+                    question = question_text,
+                    answer = str(answer)
+                )
+                question.save()
+            print 'Таблица вопросов наполнена'
+        else:
+            print 'Таблица вопросов не пустая. Создание данных пропускается'
